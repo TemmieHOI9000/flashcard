91343cb40ef3815a8e64405d15fc0dc2eb813568
TemmieHOI9000 <118019277+TemmieHOI9000@users.noreply.github.com>
2025-06-06 20:04:05 -0500

    Delete flashcard-app directory

--- flashcard-app/app/auth/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthForm from '@/components/auth/AuthForm'
-
-export default function AuthPage() {
-  return (
-    <main className="min-h-screen bg-gray-50 flex items-center justify-center">
-      <AuthForm />
-    </main>
-  )
-}
